ea7ae02c493d25bc891959f20d2b2621bd649d46
Hocks <ichakhari@gmail.com>
2023-06-18 18:00:30 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Backend/Database/__pycache__/database.cpython-39.pyc
    	modified:   Backend/Database/__pycache__/handlers.cpython-39.pyc
    	modified:   Backend/Database/__pycache__/models.cpython-39.pyc
    	modified:   Backend/Database/__pycache__/schemas.cpython-39.pyc
    	modified:   Backend/Database/database.py
    	modified:   Backend/Database/handlers.py
    	modified:   Backend/Database/schemas.py
    	modified:   Backend/__pycache__/main.cpython-39.pyc
    	modified:   Backend/databaseV0.sqlite
    	modified:   Backend/main.py

--- Backend/Database/database.py
+++ Backend/Database/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Create a sqlite engine instance
 engine = create_engine("sqlite:///databaseV0.sqlite")
+
+
+@event.listens_for(Engine, "connect")
+def set_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 
 # Create SessionLocal class from sessionmaker factory
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
--- Backend/Database/handlers.py
+++ Backend/Database/handlers.py
@@ -3,9 +3,12 @@ import Database.models as models
 from Database.database import SessionLocal
 from fastapi import status, HTTPException, Depends
 import Database.schemas as schemas
+from sqlalchemy.orm import Session
+from datetime import date
+from . import models, schemas
 
 
-class Handler():
+class Handler:
     model: models
     session: Session
 
@@ -14,107 +17,279 @@ class Handler():
         self.session = session
 
     def create(self, model_loaded):
-        # Add new entry in base table secteur
+        # Ajouter une nouvelle entrée dans la table de base
         self.session.add(model_loaded)
         self.session.commit()
         self.session.refresh(model_loaded)
-
-        # Return message with new secteur
         return model_loaded
 
     def readAll(self):
-        # Get All Entry From Table
+        # Obtenir toutes les entrées de la table
         listItem = self.session.query(self.model).all()
         return listItem
 
     def read(self, id: int):
-        # Get the item from table
+        # Obtenir l'élément de la table
         item = self.session.query(self.model).get(id)
 
-        # check if secteur item with given id exists. If not, raise exception and return 404 not found response
+        # Vérifier si l'élément avec l'ID donné existe. Sinon, générer une exception et renvoyer une réponse 404 not found
         if not item:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail=f"Id {id} is not found in table {self.model.__tablename__}")
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'ID {id} n'a pas été trouvé dans la table {self.model.__tablename__}")
 
         return item
 
     def delete(self, id: int):
-        # get the todo item with the given id
+        # Obtenir l'élément avec l'ID donné
         item = self.session.query(self.model).get(id)
 
-        # if secteur item with given id exists, delete it from the database. Otherwise raise 404 error
+        # Si l'élément avec l'ID donné existe, le supprimer de la base de données. Sinon, générer une erreur 404
         if item:
             self.session.delete(item)
             self.session.commit()
         else:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with id {item} not found")
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément avec l'ID {item} n'a pas été trouvé")
 
         return item
 
 
 class SecteurHandler(Handler):
     def create(self, secteur: schemas.SecteurCreate):
-        # Create new entry for secteur
+        # Créer une nouvelle entrée pour le secteur
         newSecteur = self.model(name_secteur=secteur.name_secteur)
         return super().create(newSecteur)
 
     def update(self, id_secteur: int, name_secteur: str):
-        # get the secteur item with the given id
+        # Obtenir l'élément du secteur avec l'ID donné
         secteur = self.session.query(self.model).get(id_secteur)
 
-        # update secteur item with the given task (if an item with the given id was found)
+        # Mettre à jour l'élément du secteur avec la tâche donnée (si un élément avec l'ID donné a été trouvé)
         if secteur:
             secteur.name_secteur = name_secteur
             self.session.commit()
 
-        # check if todo item with given id exists. If not, raise exception and return 404 not found response
+        # Vérifier si l'élément du secteur avec l'ID donné existe. Sinon, générer une exception et renvoyer une réponse 404 not found
         if not secteur:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail=f"Secteur item with id {id_secteur} not found")
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément du secteur avec l'ID {id_secteur} n'a pas été trouvé")
 
         return secteur
 
     def delete(self, id_secteur: int, force: bool = False):
-        # Check FKeys (if FORCE = True --> delete cascade)
+        # Vérifier les clés étrangères (si FORCE = True --> suppression en cascade)
         # -> Table station ?
         return super().delete(id_secteur)
 
 
 class StationHandler(Handler):
     def create(self, station: schemas.StationCreate):
-        # Check FKey exist
-        secteur = SecteurHandler(session=self.session, model=models.Secteur)
-        if secteur.read(station.id_secteur):
-
-            # Create new entry for secteur
+        try:
+            # Créer une nouvelle entrée pour la station
             newStation = self.model(name_station=station.name_station,
                                     capa_max=station.capa_max, id_secteur=station.id_secteur)
             return super().create(newStation)
+        except:
+            raise HTTPException(
+                status_code=status.HTTP_418_IM_A_TEAPOT, detail=f"Aucun secteur avec l'ID {station.id_secteur} dans la table secteur !")
 
     def update(self, id_station: int, name_station: str, capa_max: int, id_secteur: int):
-        # get the secteur item with the given id
+        # Obtenir l'élément de la station avec l'ID donné
         station = self.session.query(self.model).get(id_station)
 
-        # update station item with the given task (if an item with the given id was found)
+        # Mettre à jour l'élément de la station avec les valeurs données (si un élément avec l'ID donné a été trouvé)
         if station:
-            # Check FKey exist
-            secteur = SecteurHandler(
+            # Vérifier si la clé étrangère existe
+            secteur_handler = SecteurHandler(
                 session=self.session, model=models.Secteur)
-            if secteur.read(id_secteur):
+            if secteur_handler.read(id_secteur):
                 station.name_station = name_station
                 station.capa_max = capa_max
                 station.id_secteur = id_secteur
                 self.session.commit()
             else:
                 raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND, detail=f"Secteur item with id {id_secteur} not found")
+                    status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément du secteur avec l'ID {id_secteur} n'a pas été trouvé")
         else:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail=f"Station item with id {id_station} not found")
-        return secteur
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément de la station avec l'ID {id_station} n'a pas été trouvé")
 
-    def delete(self, id_secteur: int, force: bool = False):
-        # Check FKeys (if FORCE = True --> delete cascade)
-        # -> Table station ?
-        return super().delete(id_secteur)
+        return secteur_handler
+
+
+class ShiftHandler(Handler):
+    def create(self, shift: schemas.ShiftCreate):
+        newShift = self.model(name_shift=shift.name_shift,
+                              id_user=shift.id_user)
+        return super().create(newShift)
+
+    def update(self, id_shift: int, name_shift: str, id_user: int):
+        shift = self.session.query(self.model).get(id_shift)
+
+        if shift:
+            shift.name_shift = name_shift
+            shift.id_user = id_user
+            self.session.commit()
+
+        if not shift:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément du shift avec l'ID {id_shift} n'a pas été trouvé")
+
+        return shift
+
+
+class UserHandler(Handler):
+    def create(self, user: schemas.UserCreate):
+        new_user = self.model(
+            id_card=user.id_card,
+            login=user.login,
+            password=user.password,
+            start_date=user.start_date,
+            end_date=user.end_date
+        )
+        return super().create(new_user)
+
+    def update(self, id_user: int, id_card: str, login: str, password: str, start_date: date, end_date: date):
+        user = self.session.query(self.model).get(id_user)
+
+        if user:
+            user.id_card = id_card
+            user.login = login
+            user.password = password
+            user.start_date = start_date
+            user.end_date = end_date
+            self.session.commit()
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément de l'utilisateur avec l'ID {id_user} n'a pas été trouvé")
+
+        return user
+
+
+class OperateurHandler(Handler):
+    def create(self, operateur: schemas.OperateurCreate):
+        new_operateur = self.model(
+            id_card=operateur.id_card,
+            name_operateur=operateur.name_operateur,
+            id_shift=operateur.id_shift,
+            home_station=operateur.home_station,
+            start_date=operateur.start_date,
+            end_date=operateur.end_date,
+            isTemp=operateur.isTemp,
+            active_status=operateur.active_status
+        )
+        return super().create(new_operateur)
+
+    def update(self, id_operateur: int, id_card: int, name_operateur: str, id_shift: int, home_station: int,
+               start_date: date, end_date: date, isTemp: bool, active_status: bool):
+        operateur = self.session.query(self.model).get(id_operateur)
+
+        if operateur:
+            operateur.id_card = id_card
+            operateur.name_operateur = name_operateur
+            operateur.id_shift = id_shift
+            operateur.home_station = home_station
+            operateur.start_date = start_date
+            operateur.end_date = end_date
+            operateur.isTemp = isTemp
+            operateur.active_status = active_status
+            self.session.commit()
+
+        if not operateur:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément de l'opérateur avec l'ID {id_operateur} n'a pas été trouvé")
+
+        return operateur
+
+
+class CompetenceHandler(Handler):
+    def create(self, competence: schemas.CompetenceCreate):
+        new_competence = self.model(
+            id_station=competence.id_station,
+            level_competence=competence.level_competence,
+            last_assesement=competence.last_assesement,
+            id_operateur=competence.id_operateur
+        )
+        return super().create(new_competence)
+
+    def update(self, id_competence: int, id_station: int, level_competence: int, last_assesement: date, id_operateur: int):
+        competence = self.session.query(self.model).get(id_competence)
+
+        if competence:
+            competence.id_station = id_station
+            competence.level_competence = level_competence
+            competence.last_assesement = last_assesement
+            competence.id_operateur = id_operateur
+            self.session.commit()
+
+        if not competence:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"L'élément de compétence avec l'ID {id_competence} n'a pas été trouvé")
+
+        return competence
+
+
+class SoftCompetenceHandler(Handler):
+    def create(self, soft_competence: schemas.SoftCompetenceCreate):
+        new_soft_competence = self.model(
+            id_station=soft_competence.id_station,
+            level_competence=soft_competence.level_competence,
+            last_assesement=soft_competence.last_assesement,
+            id_operateur=soft_competence.id_operateur
+        )
+        return super().create(new_soft_competence)
+
+    def update(self, id_soft_competence: int, id_station: int, level_competence: int, last_assesement: date,
+               id_operateur: int):
+        soft_competence = self.session.query(
+            self.model).get(id_soft_competence)
+
+        if soft_competence:
+            soft_competence.id_station = id_station
+            soft_competence.level_competence = level_competence
+            soft_competence.last_assesement = last_assesement
+            soft_competence.id_operateur = id_operateur
+            self.session.commit()
+
+        if not soft_competence:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"L'élément de compétence soft avec l'ID {id_soft_competence} n'a pas été trouvé")
+
+        return soft_competence
+
+
+class PlanningHandler(Handler):
+    def create(self, planning: schemas.PlanningCreate):
+        new_planning = self.model(
+            id_operateur=planning.id_operateur,
+            id_user=planning.id_user,
+            id_shift=planning.id_shift,
+            id_station=planning.id_station,
+            date=planning.date,
+            week=planning.week,
+            day=planning.day
+        )
+        return super().create(new_planning)
+
+    def update(self, id_planning: int, id_operateur: int, id_user: int, id_shift: int, id_station: int, date: date,
+               week: int, day: int):
+        planning = self.session.query(self.model).get(id_planning)
+
+        if planning:
+            planning.id_operateur = id_operateur
+            planning.id_user = id_user
+            planning.id_shift = id_shift
+            planning.id_station = id_station
+            planning.date = date
+            planning.week = week
+            planning.day = day
+            self.session.commit()
+
+        if not planning:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"L'élément du planning avec l'ID {id_planning} n'a pas été trouvé")
+
+        return planning
