4318b9e0cee1e4e8d72da807156e79c4945dd178
Hocks <ichakhari@gmail.com>
2023-08-05 13:56:08 +0200

    Changes to be committed: 	new file:   Backend/ETLs.py
     Changes not staged for commit:
    	modified:   Backend/Database/__pycache__/database.cpython-39.pyc
    	modified:   Backend/Database/__pycache__/models.cpython-39.pyc
    	modified:   Backend/Database/__pycache__/schemas.cpython-39.pyc
    	modified:   Backend/Database/database.py
    	modified:   Backend/Database/models.py
    	modified:   Backend/Database/schemas.py
    	modified:   Backend/__pycache__/main.cpython-39.pyc
    
     Untracked files:
    	Backend/databaseV4.sqlite
    	DataLastWeek/
    	Documentation/JeuTest/~$V1.xlsx

---
+++ Backend/ETLs.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm import Session
+import Database.models as models
+from Database.database import SessionLocal
+from fastapi import status, HTTPException, Depends
+import Database.schemas as schemas
+from sqlalchemy.orm import Session
+from datetime import date
+import pandas as pd
+from pandas import DataFrame
+
+# * update operateur
+# * delete n reload competence
+# * delete n reload softcompetence
+# * add ke
+# * add qty
+
+
+class ETL_Loading:
+    session: Session
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def extract(self, dataSourcePath: DataFrame, target: models):
+        pass
+
+    def transform():
+        pass
+
+    def load():
+        pass
+
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+if __name__ == "__main__":
+    print("looo")
